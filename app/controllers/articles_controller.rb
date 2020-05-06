1ce44405a3a9e808afc34292278b227ac86e407a
abhishekyadav09 <abhishek.yadav090@gmail.com>
2020-05-05 23:24:36 -0700

    Created the show all articles features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,10 @@ class ArticlesController < ApplicationController
 
   #this is my controller
 
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
