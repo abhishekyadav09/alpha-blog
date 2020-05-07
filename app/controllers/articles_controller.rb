0504de595cf4058c83b7b303088f7b1a2fc76a4a
abhishekyadav09 <abhishek.yadav090@gmail.com>
2020-05-06 21:28:27 -0700

    Edit functionality is working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,7 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+
   def show
     @article = Article.find(params[:id])
   end
@@ -13,6 +14,11 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
@@ -23,7 +29,16 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
 end
